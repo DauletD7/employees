846535073984a3d979b7e378a9fd52dbeb233df6
DauletD7 <dauletd7@mail.ru>
2023-02-21 13:22:39 +0600

    first

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,27 +1,68 @@
-import "./employees-add-form.css";
+import { Component } from "react";
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="З/П в $?"
-        />
+// import "./employees-add-form.css";
+import "./employees-add-form.scss";
 
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+class EmployeesAddForm extends Component {
+  state = {
+    name: "",
+    salary: "",
+  };
 
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.name.length < 3 || !this.state.salary) return;
+    this.props.onAdd(this.state.name, this.state.salary);
+    this.setState({
+      name: "",
+      salary: "",
+    });
+  };
+
+  // static onLog = () => {
+  //   console.log("Hey");
+  // };
+
+  // static logged = "on";
+
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex" onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            className="form-control new-post-label"
+            name="name"
+            value={name}
+            placeholder="Как его зовут?"
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            name="salary"
+            value={salary}
+            placeholder="З/П в $?"
+            onChange={this.onValueChange}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+// EmployeesAddForm.onLog();
+// console.log(EmployeesAddForm.logged);
 export default EmployeesAddForm;
